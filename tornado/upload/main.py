af944cb18c762e2d2d47c59af8cb77ae1c3299d0
wtmmac <wtmmac@gmail.com>
2013-11-09 09:49:22 +0800

    ignore upload directory

--- tornado/upload/main.py
+++ tornado/upload/main.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 # coding=utf-8
-import tornado.ioloop  
-import tornado.web  
+import tornado.ioloop
+import tornado.web
 import os, datetime, time
 
 APP_ROOT = os.path.dirname(__file__)
 
-  
-class MainHandler(tornado.web.RequestHandler):  
+class MainHandler(tornado.web.RequestHandler):
     def get(self):  
-        self.render(os.path.join(APP_ROOT, 'index.html'))  
+        self.render(os.path.join(APP_ROOT, 'index.html'))
   
-class UploadHandler(tornado.web.RequestHandler):  
+class UploadHandler(tornado.web.RequestHandler):
     def post(self):  
         if self.request.files:  
             uploadFile = self.request.files['myfile'][0]
-            fileName =  time.strftime("%Y%m%d%H%M%S") + '%d' % datetime.datetime.now().microsecond + '.' + uploadFile["filename"].split('.')[-1].lower() 
-            fin = open(os.path.join(APP_ROOT, "upload", fileName),"w")    
-            fin.write(uploadFile["body"])  
-            fin.close()
-            self.write("uploaded!")
-            print(uploadFile)
+            fileName =  time.strftime("%Y%m%d%H%M%S") + '%d' % datetime.datetime.now().microsecond + '.' + uploadFile["filename"].split('.')[-1].lower()
+            uploadPath = time.strftime("%Y/%m/%d")
+            uploadRealPath = os.path.join(APP_ROOT, "upload", uploadPath)
+            try:
+                os.makedirs(uploadRealPath)
+            except Exception, e:
+                pass
+            uploadRealFilePath = os.path.join(uploadRealPath, fileName)
+            uploadedFile = open(uploadRealFilePath, "w")
+            uploadedFile.write(uploadFile["body"])
+            uploadedFile.close()
+            self.write('{"status":200, "file":"%s"}' % os.path.join(uploadPath, fileName))
 
 settings = {
     "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
@@ -29,12 +34,12 @@ settings = {
     "debug": True,
 }
 
-application=tornado.web.Application([
+application = tornado.web.Application([
     (r'/',MainHandler),
     #(),
-    (r'/upload', UploadHandler) 
-], **settings)  
-  
-if __name__=='__main__':  
+    (r'/upload', UploadHandler)
+], **settings)
+
+if __name__ == '__main__':
     application.listen(8001)
     tornado.ioloop.IOLoop.instance().start()
